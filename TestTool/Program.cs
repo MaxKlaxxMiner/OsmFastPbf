6ee6df02d8180cb54ea92cc26b97ae491cfbddcb
MaxKlaxx <max@tetrisk.de>
2020-09-28 03:34:24 +0200

    decode stringtable

--- TestTool/Program.cs
+++ TestTool/Program.cs
@@ -9,22 +9,22 @@ namespace TestTool
 {
   partial class Program
   {
-    static int DecodeStringTable(byte[] buf, int ofs)
+    static int DecodeStringTable(byte[] buf, int ofs, out string[] val)
     {
       int len = 0;
-      //byte t = buf[ofs + len++];
-      //if (t != (2 | 1 << 3)) throw new PbfParseException(); // Length-delimited (2), string (1)
-      //ulong dataLen;
-      //len += ProtoBuf.ReadVarInt(buf, ofs + len, out dataLen);
-      //int endLen = len + (int)dataLen;
-      //var stringTable = new List<string>();
-      //for (; len < endLen; )
-      //{
-      //  string tmp;
-      //  len += ProtoBuf.ReadString(buf, ofs + len, out tmp);
-      //  stringTable.Add(tmp);
-      //}
-      //if (len != endLen) throw new PbfParseException();
+      ulong dataLen;
+      len += ProtoBuf.ReadVarInt(buf, ofs + len, out dataLen);
+      int endLen = len + (int)dataLen;
+      var stringTable = new List<string>();
+      for (; len < endLen; )
+      {
+        string tmp;
+        if (buf[ofs + len++] != (1 << 3 | 2)) throw new PbfParseException();
+        len += ProtoBuf.ReadString(buf, ofs + len, out tmp);
+        stringTable.Add(tmp);
+      }
+      if (len != endLen) throw new PbfParseException();
+      val = stringTable.ToArray();
       return len;
     }
 
@@ -98,13 +98,16 @@ namespace TestTool
       int len = 0;
 
       // --- required StringTable stringtable = 1; ---
-      len += DecodeStringTable(buf, ofs + len);
+      if (buf[ofs + len++] != (1 << 3 | 2)) throw new PbfParseException();
+      string[] stringTable;
+      len += DecodeStringTable(buf, ofs + len, out stringTable);
 
       // --- repeated PrimitiveGroup primitivegroup = 2; ---
-      if (buf[ofs + len] == (2 | 2 << 3))
+      while (buf[ofs + len] == (2 << 3 | 2))
       {
+        len++;
         ulong dataLen;
-        len += ProtoBuf.ReadVarInt(buf, ofs + ++len, out dataLen);
+        len += ProtoBuf.ReadVarInt(buf, ofs + len, out dataLen);
         int endLen = len + (int)dataLen;
         for (; len < endLen; len++)
         {
@@ -127,113 +130,10 @@ namespace TestTool
     {
       //BufferTest();
 
-      #region # // --- old ---
-      //static int ReadEmbedded(byte[] buf, int ofs, out byte[] val)
-      //{
-      //  int len = 0;
-      //  byte t = buf[ofs + len++];
-      //  if (t != (2 | 3 << 3)) throw new PbfParseException(); // Length-delimited (2), embedded message (3)
-      //  int size = buf[ofs + len++];
-      //  if (size > 127)
-      //  {
-      //    size &= 127;
-      //    for (int bit = 7; ; bit += 7)
-      //    {
-      //      byte b = buf[ofs + len++];
-      //      size |= (b & 127) << bit;
-      //      if (b <= 127) break;
-      //    }
-      //  }
-      //  val = new byte[size];
-      //  Array.Copy(buf, ofs + len, val, 0, size); len += size;
-      //  return len;
-      //}
-      //static int ReadEmbeddedCompressed(byte[] buf, int ofs, out byte[] val)
-      //{
-      //  int len = ReadEmbedded(buf, ofs, out val);
-      //  using (var zlib = new InflaterInputStream(new RamStream(val))) val = zlib.ReadAllBytes(true);
-      //  return len;
-      //}
-
-      //static int ReadPacket(byte[] buf, int ofs, out List<ulong> val)
-      //{
-      //  int len = 0;
-      //  byte t = buf[ofs + len++];
-      //  if (t != (2 | 4 << 3) && t != (2 | 1 << 3) && t != (2 | 5 << 3) && t != (2 | 16 << 3)) throw new PbfParseException(); // Length-delimited (2), *
-      //  int size = buf[ofs + len++];
-      //  if (size > 127)
-      //  {
-      //    size &= 127;
-      //    for (int bit = 7; ; bit += 7)
-      //    {
-      //      byte b = buf[ofs + len++];
-      //      size |= (b & 127) << bit;
-      //      if (b <= 127) break;
-      //    }
-      //  }
-      //  int end = len + size;
-      //  var vals = new List<ulong>();
-      //  while (len < end)
-      //  {
-      //    ulong v = buf[ofs + len++];
-      //    if (v > 127)
-      //    {
-      //      v &= 127;
-      //      for (int bit = 7; ; bit += 7)
-      //      {
-      //        byte b = buf[ofs + len++];
-      //        v |= (ulong)(b & 127) << bit;
-      //        if (b <= 127) break;
-      //      }
-      //    }
-      //    vals.Add(v);
-      //  }
-      //  val = vals;
-      //  return len;
-      //}
-      //static int ReadPacket(byte[] buf, int ofs, out string val)
-      //{
-      //  List<ulong> tmp;
-      //  int len = ReadPacket(buf, ofs, out tmp);
-      //  val = new string(tmp.SelectArray(c => checked((char)c)));
-      //  return len;
-      //}
-      //static int ReadPacket(byte[] buf, int ofs, out ulong[] val)
-      //{
-      //  List<ulong> tmp;
-      //  int len = ReadPacket(buf, ofs, out tmp);
-      //  val = tmp.ToArray();
-      //  return len;
-      //}
-      //static int ReadPacket(byte[] buf, int ofs, string val)
-      //{
-      //  string tmp;
-      //  int len = ReadPacket(buf, ofs, out tmp);
-      //  if (tmp != val) throw new PbfParseException();
-      //  return len;
-      //}
-
-      //public struct OSMHeader
-      //{
-      //  public OSMHeader(byte[] buf)
-      //  {
-      //    using (var zlib = new InflaterInputStream(new RamStream(buf))) buf = zlib.ReadAllBytes(true);
-
-      //    int pos = 0;
-      //    ulong[] bbox;
-      //    pos += ReadPacket(buf, pos, out bbox);
-      //    pos += ReadPacket(buf, pos, "OsmSchema-V0.6");
-      //    pos += ReadPacket(buf, pos, "DenseNodes");
-      //    pos += ReadPacket(buf, pos, "Has_Metadata");
-      //    pos += ReadPacket(buf, pos, "Sort.Type_then_ID");
-      //    pos += ReadPacket(buf, pos, "\x14");
-      //  }
-      //}
-      #endregion
-
       string path = "planet-latest.osm.pbf";
       for (int i = 0; i < 32 && !File.Exists(path); i++) path = "../" + path;
       if (!File.Exists(path)) throw new FileNotFoundException(path.TrimStart('.', '/'));
+
       using (var test = new FastPbfReader(path))
       {
         #region # // --- Index einlesen ---
@@ -278,11 +178,11 @@ namespace TestTool
           if (blob.IsHeader)
           {
             HeaderBlock headerBlock;
-            len = HeaderBlock.Decode(outputBuf, ofs, out headerBlock);
+            len = HeaderBlock.Decode(outputBuf, 0, out headerBlock);
           }
           else
           {
-            len = DecodePrimitiveBlock(outputBuf, ofs);
+            len = DecodePrimitiveBlock(outputBuf, 0);
           }
           if (len != bytes) throw new PbfParseException();
         }
