643a065cffbb79462cc3b9422221e923b6795ff8
MaxKlaxx <max@tetrisk.de>
2020-12-15 07:52:00 +0100

    +simple node writer

--- TestTool/Program.cs
+++ TestTool/Program.cs
@@ -30,7 +30,8 @@ namespace TestTool
 
       using (var pbf = new OsmPbfReader(PbfPath))
       {
-        long testRelationID = 418713; // Herrnhut (Germany)
+        //long testRelationID = 418713; // Herrnhut (Germany)
+        long testRelationID = 62422; // Berlin
         //long testRelationID = 62504; // Brandenburg (Germany) - https://www.openstreetmap.org/relation/62504
         //long testRelationID = 1434381; // Insel Rügen (Germany)
         //long testRelationID = 51477; // Germany
@@ -54,10 +55,53 @@ namespace TestTool
         }
 
         var ways = pbf.ReadWays(relations.SelectMany(r => r.members.Where(x => x.type == MemberType.Way).Select(x => x.id)).ToArray());
+        Array.Sort(ways, (x, y) => x.id.CompareTo(y.id));
 
         var nodes = pbf.ReadNodes(relations.SelectMany(r => r.members.Where(x => x.type == MemberType.Node).Select(x => x.id)).Concat(ways.SelectMany(w => w.nodeIds)).ToArray());
+        Array.Sort(nodes, (x, y) => x.id.CompareTo(y.id));
 
         Console.WriteLine("nodes: {0:N0}", nodes.Length);
+
+        var knownNodes = new HashSet<long>();
+        Func<StreamWriter, long, string, int> writePath = null;
+        writePath = (wdat, relationId, type) =>
+        {
+          int count = 0;
+          var rel = relations.First(x => x.id == relationId);
+          foreach (var member in rel.members)
+          {
+            switch (member.type)
+            {
+              case MemberType.Node: break; // skip
+              case MemberType.Way:
+              {
+                if (member.role == type)
+                {
+                  var way = ways.BinarySearchSingle(w => w.id - member.id);
+                  foreach (var nodeId in way.nodeIds)
+                  {
+                    if (knownNodes.Contains(nodeId)) continue;
+                    knownNodes.Add(nodeId);
+                    var node = nodes.BinarySearchSingle(n => n.id - nodeId);
+                    wdat.WriteLine(node.latCode + "," + node.lonCode);
+                    count++;
+                  }
+                }
+              } break;
+              case MemberType.Relation: count += writePath(wdat, member.id, type); break;
+            }
+          }
+          return count;
+        };
+
+        using (var wdat = new StreamWriter(testRelationID + "_outer.txt"))
+        {
+          writePath(wdat, testRelationID, "outer");
+        }
+        using (var wdat = new StreamWriter(testRelationID + "_inner.txt"))
+        {
+          writePath(wdat, testRelationID, "inner");
+        }
       }
     }
   }
